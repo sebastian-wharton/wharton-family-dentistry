b342a441ccf3bade40bf3e179349bdfe0636352e
Sebastian Wharton <sebastian.wharton1@gmail.com>
2022-08-05 23:28:05 -0500

    file layout, extract components 8/6

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h2>
-        Wharton Family Dentistry is up & running with Tailwind & Next JS!!
-      </h2>
-    </div>
+    <>
+      <h1>Hello World!</h1>
+      <p className="text-3xl font-bold underline">
+        Hello world! Wharton Family Dentistry is up & running with Tailwind &
+        Next JS!!
+      </p>
+    </>
   );
 }
